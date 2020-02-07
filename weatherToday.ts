553a0441d303a099030ac7304adc02bbad23c284
rettar5 <rettar.v@gmail.com>
2020-02-07 20:44:36 +0900

    Fixed bug of can not post weather forecast when duplicate posting time.

--- weatherToday.ts
+++ weatherToday.ts
@@ -1,14 +1,18 @@
-import { Log, OdnUtils } from "../../../odnUtils";
-import { AccountConfigs, AccountData } from "../../../configs/accountConfigs";
-import { OdnPlugins } from "../../../odnPlugins";
-import { OdnTweets } from "../../../odnTweets";
-import * as Forecast from "forecast";
+import { Log, OdnUtils } from '../../../odnUtils';
+import { AccountConfigs, AccountData } from '../../../configs/accountConfigs';
+import { OdnPlugins } from '../../../odnPlugins';
+import { OdnTweets } from '../../../odnTweets';
+import * as Forecast from 'forecast';
 
 export class WeatherToday {
   private static scheduleList: WeatherTodaySchedule[];
   private static forecast: Forecast;
 
-  constructor(private accountData: AccountData, private nowDate: Date, private fullName: string) {}
+  constructor(
+    private accountData: AccountData,
+    private nowDate: Date,
+    private fullName: string
+  ) {}
 
   /**
    * プラグインのメイン処理を実行
@@ -16,10 +20,30 @@ export class WeatherToday {
    * @param {(isProcessed?: boolean) => void} finish
    */
   run(finish: (isProcessed?: boolean) => void) {
-    const schedule = WeatherToday.getMatchedSchedule(this.nowDate, WeatherToday.scheduleList);
-    this.tweetWeather(schedule,(isSuccess) => {
-      finish(isSuccess);
+    const schedules = WeatherToday.getMatchedSchedule(
+      this.nowDate,
+      WeatherToday.scheduleList
+    );
+    Log.d('schedules: ', schedules.map(s => s.screenName).join(','));
+    const promiseList = schedules.map(schedule => {
+      return new Promise((res, rej) => {
+        this.tweetWeather(schedule, isSuccess => {
+          if (isSuccess) {
+            res();
+          } else {
+            rej();
+          }
+        });
+      });
     });
+
+    Promise.all(promiseList)
+      .then(() => {
+        finish(true);
+      })
+      .catch(e => {
+        finish(false);
+      });
   }
 
   /**
@@ -39,33 +63,43 @@ export class WeatherToday {
       Log.t('forecast', this.forecast);
     }
 
-    return this.getMatchedSchedule(nowDate, this.scheduleList) ? true : false;
+    return 0 < this.getMatchedSchedule(nowDate, this.scheduleList)?.length
+      ? true
+      : false;
   }
 
-  private static getSchedules(accountData: AccountData): WeatherTodaySchedule[] {
+  private static getSchedules(
+    accountData: AccountData
+  ): WeatherTodaySchedule[] {
     const key = `${WeatherTodayEnvKey.Schedules}_${accountData.userId}`;
     const raw = OdnPlugins.getEnvData(WeatherTodayEnvKey.FullName, key);
     let result: WeatherTodaySchedule[] = [];
     try {
       result = JSON.parse(raw);
-    } catch(e) {
-      Log.w("Invalid environments data format.", e);
+    } catch (e) {
+      Log.w('Invalid environments data format.', e);
     }
     return result;
   }
 
-  private static getMatchedSchedule(nowDate: Date, scheduleList: WeatherTodaySchedule[]): WeatherTodaySchedule {
+  private static getMatchedSchedule(
+    nowDate: Date,
+    scheduleList: WeatherTodaySchedule[]
+  ): WeatherTodaySchedule[] {
     const hours = nowDate.getHours();
     const minutes = nowDate.getMinutes();
-    return scheduleList.find((s) => {
+    return scheduleList.filter(s => {
       return s && hours === s.hours && minutes === s.minutes;
     });
   }
 
   private static getForecastKey(accountData: AccountData): string {
-    const key = OdnPlugins.getEnvData(WeatherTodayEnvKey.FullName, `${WeatherTodayEnvKey.ForecastKey}_${accountData.userId}`);
+    const key = OdnPlugins.getEnvData(
+      WeatherTodayEnvKey.FullName,
+      `${WeatherTodayEnvKey.ForecastKey}_${accountData.userId}`
+    );
     if (!key) {
-      Log.w("Could not load forecast key from environments.");
+      Log.w('Could not load forecast key from environments.');
     }
     return key;
   }
@@ -75,59 +109,68 @@ export class WeatherToday {
       return null;
     }
     return new Forecast({
-      service: "darksky",
+      service: 'darksky',
       key: key,
-      units: "celcius",
-      cache: true,      // Cache API requests
-      ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+      units: 'celcius',
+      cache: true, // Cache API requests
+      ttl: {
+        // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
         minutes: 27,
         seconds: 45
       }
     });
   }
 
-  private tweetWeather(schedule: WeatherTodaySchedule, cb: (isSuccess: boolean) => void) {
-    WeatherToday.forecast.get(schedule.location.point, (error, weather: WeatherForcast) => {
-      if (error) {
-        Log.e("Error occurred at getting forecast resources.", error);
-        return;
-      }
-
-      /** ツイートする予報の間隔 */
-      const inter = 3;
-      /** ツイートする予報の数 */
-      const count = 8;
-      let text = '';
-
-      if (inter * count < (weather?.hourly?.data?.length || 0)) {
-        const now = new Date(weather?.currently?.time * 1000);
-        text += `${schedule.location.name}の天気予報\n\n`;
-        // 3時間おきの天気
-        for (let i = 0; i < count; i++) {
-          const data = weather?.hourly?.data?.[i * inter];
-          const time = new Date(data.time * 1000);
-          const temp = Math.round(data.temperature);
-          const precipProbability = Math.round(data.precipProbability * 100);
-          text += `${time.getHours()}時\n`;
-          text += `${this.getEmoji(data.icon)} ${temp}℃ ${precipProbability}％\n`;
-          text += `\n`;
+  private tweetWeather(
+    schedule: WeatherTodaySchedule,
+    cb: (isSuccess: boolean) => void
+  ) {
+    WeatherToday.forecast.get(
+      schedule.location.point,
+      (error, weather: WeatherForcast) => {
+        if (error) {
+          Log.e('Error occurred at getting forecast resources.', error);
+          return;
         }
-      } else {
-        text = `天気予報の取得に失敗しました。`;
-      }
 
-      // 文字数制限
-      if (140 < text.length) {
-        text = text.substr(0, 139);
-        text += `…`;
-      }
+        /** ツイートする予報の間隔 */
+        const inter = 3;
+        /** ツイートする予報の数 */
+        const count = 8;
+        let text = '';
 
-      const tweets = new OdnTweets(this.accountData);
-      tweets.text = text;
-      tweets.postTweet((isSuccess) => {
-        cb(isSuccess);
-      });
-    });
+        if (inter * count < (weather?.hourly?.data?.length || 0)) {
+          const now = new Date(weather?.currently?.time * 1000);
+          text += `${schedule.location.name}の天気予報\n\n`;
+          // 3時間おきの天気
+          for (let i = 0; i < count; i++) {
+            const data = weather?.hourly?.data?.[i * inter];
+            const time = new Date(data.time * 1000);
+            const temp = Math.round(data.temperature);
+            const precipProbability = Math.round(data.precipProbability * 100);
+            text += `${time.getHours()}時\n`;
+            text += `${this.getEmoji(
+              data.icon
+            )} ${temp}℃ ${precipProbability}％\n`;
+            text += `\n`;
+          }
+        } else {
+          text = `天気予報の取得に失敗しました。`;
+        }
+
+        // 文字数制限
+        if (140 < text.length) {
+          text = text.substr(0, 139);
+          text += `…`;
+        }
+
+        const tweets = new OdnTweets(this.accountData);
+        tweets.text = text;
+        tweets.postTweet(isSuccess => {
+          cb(isSuccess);
+        });
+      }
+    );
   }
 
   private getEmoji(icon: WeatherIconTypes): string {
@@ -150,93 +193,103 @@ interface Location {
 }
 
 enum WeatherTodayEnvKey {
-  FullName = "PLUGINSBATCHWEATHERTODAY", // プラグイン実行チェック時はプラグイン名が参照できないため定数として定義
-  Schedules = "SCHEDULES",
-  ForecastKey = "FORECAST_KEY"
+  FullName = 'PLUGINSBATCHWEATHERTODAY', // プラグイン実行チェック時はプラグイン名が参照できないため定数として定義
+  Schedules = 'SCHEDULES',
+  ForecastKey = 'FORECAST_KEY'
 }
 
 const WeatherIcons = {
   'clear-day': '☀',
   'clear-night': '🌙',
-  'rain': '🌧',
-  'snow': '☃',
-  'sleet': '🌨',
-  'wind': '💨',
-  'fog': '🌫',
-  'cloudy': '☁',
+  rain: '🌧',
+  snow: '☃',
+  sleet: '🌨',
+  wind: '💨',
+  fog: '🌫',
+  cloudy: '☁',
   'partly-cloudy-day': '⛅',
   'partly-cloudy-night': '☁'
-}
+};
 
 interface WeatherForcast {
-  'currently': Weather;
-  'daily': {
-    'data': WeatherDetail[];
-    'icon': WeatherIconTypes;
-    'summary': string,
+  currently: Weather;
+  daily: {
+    data: WeatherDetail[];
+    icon: WeatherIconTypes;
+    summary: string;
   };
-  'expires': number;
-  'flags': {
+  expires: number;
+  flags: {
     'nearest-station': number;
-    'sources': string[];
-    'units': string;
+    sources: string[];
+    units: string;
   };
-  'hourly': {
-    'data': Weather[];
-    'icon': WeatherIconTypes;
-    'summary': string,
-  }
-  'latitude': number;
-  'longitude': number;
-  'offset': number;
-  'timezone': string;
+  hourly: {
+    data: Weather[];
+    icon: WeatherIconTypes;
+    summary: string;
+  };
+  latitude: number;
+  longitude: number;
+  offset: number;
+  timezone: string;
 }
 
 interface Weather {
-  'apparentTemperature': number;
-  'cloudCover': number;
-  'dewPoint': number;
-  'humidity': number;
-  'icon': WeatherIconTypes;
-  'ozone': number;
-  'precipIntensity': number;
-  'precipProbability': number;
-  'precipType': string;
-  'pressure': number;
-  'summary': string;
-  'temperature': number;
-  'time': number;
-  'uvIndex': number;
-  'visibility': number;
-  'windBearing': number;
-  'windGust': number;
-  'windSpeed': number;
+  apparentTemperature: number;
+  cloudCover: number;
+  dewPoint: number;
+  humidity: number;
+  icon: WeatherIconTypes;
+  ozone: number;
+  precipIntensity: number;
+  precipProbability: number;
+  precipType: string;
+  pressure: number;
+  summary: string;
+  temperature: number;
+  time: number;
+  uvIndex: number;
+  visibility: number;
+  windBearing: number;
+  windGust: number;
+  windSpeed: number;
 }
 
 interface WeatherDetail extends Weather {
-  "apparentTemperatureHigh": number;
-  "apparentTemperatureHighTime": number;
-  "apparentTemperatureLow": number;
-  "apparentTemperatureLowTime": number;
-  "apparentTemperatureMax": number;
-  "apparentTemperatureMaxTime": number;
-  "apparentTemperatureMin": number;
-  "apparentTemperatureMinTime": number;
-  "moonPhase": number;
-  "precipIntensityMax": number;
-  "precipIntensityMaxTime": number;
-  "sunriseTime": number;
-  "sunsetTime": number;
-  "temperatureHigh": number;
-  "temperatureHighTime": number;
-  "temperatureLow": number;
-  "temperatureLowTime": number;
-  "temperatureMax": number;
-  "temperatureMaxTime": number;
-  "temperatureMin": number;
-  "temperatureMinTime": number;
-  "uvIndexTime": number;
-  "windGustTime": number;
+  apparentTemperatureHigh: number;
+  apparentTemperatureHighTime: number;
+  apparentTemperatureLow: number;
+  apparentTemperatureLowTime: number;
+  apparentTemperatureMax: number;
+  apparentTemperatureMaxTime: number;
+  apparentTemperatureMin: number;
+  apparentTemperatureMinTime: number;
+  moonPhase: number;
+  precipIntensityMax: number;
+  precipIntensityMaxTime: number;
+  sunriseTime: number;
+  sunsetTime: number;
+  temperatureHigh: number;
+  temperatureHighTime: number;
+  temperatureLow: number;
+  temperatureLowTime: number;
+  temperatureMax: number;
+  temperatureMaxTime: number;
+  temperatureMin: number;
+  temperatureMinTime: number;
+  uvIndexTime: number;
+  windGustTime: number;
 }
 
-type WeatherIconTypes = 'clear-day' | 'clear-night' | 'rain' | 'snow' | 'sleet' | 'wind' | 'fog' | 'cloudy' | 'partly-cloudy-day' | 'partly-cloudy-night';
+type WeatherIconTypes =
+  | 'clear-day'
+  | 'clear-night'
+  | 'rain'
+  | 'snow'
+  | 'sleet'
+  | 'wind'
+  | 'fog'
+  | 'cloudy'
+  | 'partly-cloudy-day'
+  | 'partly-cloudy-night';
